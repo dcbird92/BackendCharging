faf8a291536f6510da5799ee29c9eb284080623a
Sean Hammond <dcbird92@gmail.com>
2026-01-07 16:35:37 +0100

    Build Razor Pages dashboard layout

---
+++ EVCharging/Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EVCharging.Pages;
+
+/// <summary>
+/// Landing page model for the Razor Pages GUI.
+/// </summary>
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
